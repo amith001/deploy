b64de5bc2c6684945c6be5047b8aeb494326eb70
amith001 <vinayakamith10@gmail.com>
2019-02-17 14:34:06 +0530

    Updates

---
+++ precache-manifest.8d5db513e25a57d33fef4dbe71061802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b6fa2accf8ca3d496af",
+    "url": "/deploy/static/css/main.f2c99f3c.chunk.css"
+  },
+  {
+    "revision": "5b6fa2accf8ca3d496af",
+    "url": "/deploy/static/js/main.5b6fa2ac.chunk.js"
+  },
+  {
+    "revision": "de96f4ccd24f5c103fa3",
+    "url": "/deploy/static/js/runtime~main.de96f4cc.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/deploy/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6bdad71dd287a401c7d65a9fb75aca31",
+    "url": "/deploy/index.html"
+  }
+];
